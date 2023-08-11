99162c8223282659f675dd643a39e53302c5a2ad
Ender <78042486+Sylvie-TN@users.noreply.github.com>
2023-08-11 00:33:55 -0400

    fix routing util

--- lib/global/util/route.ts
+++ lib/global/util/route.ts
@@ -1,8 +1,25 @@
 import DynamicUtil from "../util";
 
 async function route(this: DynamicUtil, request: Request) {
-  var parsed = new URL(request.url);
-  var url = parsed.searchParams.get('url');
+  var url;
+
+  if (request.method === "GET") {
+    var parsed = new URL(request.url);
+    url = parsed.searchParams.get('url');
+  } else if (request.method === "POST") {
+    const formData = await request.formData();
+
+    url = formData.get('url');
+
+    if (url === null) {
+      var parsed = new URL(request.url);
+      url = parsed.searchParams.get('url');
+    }
+
+    if (!url) return new Response('Error: Invalid or Unfound url', {status: 400});
+  } else {
+    return new Response('Error: Invalid method', {status: 405});
+  }
 
   return new Response('', {status: 301, headers: {location: location.origin+this.ctx.config.prefix+this.ctx.encoding.encode(url)}});
 }
