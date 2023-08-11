8986a7af3b6f9b466bb16270808b070e74e61e5e
Ender <78042486+Sylvie-TN@users.noreply.github.com>
2023-08-10 23:23:08 -0400

    small style things

--- static/resources/scripts/notice.js
+++ static/resources/scripts/notice.js
@@ -2,7 +2,7 @@ const delay = ms => new Promise(res => setTimeout(res, ms));
 
 
 async function greet(){
-    const style = 'background-color: black; color: white; font-style: italic; border: 5px solid red; font-size: 2em;'
+    const style = 'background-color: black; color: white; font-style: italic; border: 5px solid red; font-size: 2em; margin-top: -12px;'
     let visited = localStorage.getItem('visited')
     await delay(500);
     console.log("%cPlease, do not put anything in this developer console. You risk your data.", style)
